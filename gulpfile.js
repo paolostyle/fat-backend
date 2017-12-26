75a85f3e3e1a50487305f8a53791c097d45763b4
paolostyle <tuptus95@gmail.com>
2017-12-26 11:13:26 +0100

    Skeleton

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('develop', () => {
+    nodemon({
+        exec: 'node --debug',
+        script: 'server.js',
+        ext: 'js',
+        tasks: ['lint'],
+        env: {'NODE_ENV': 'development'}
+    });
+});
